37efa3a2f26570e66d1ad73c4d070704cdeb5747
Neil Green <ngreen@allvuesystems.com>
2025-12-10 15:57:03 -0800

    Fix issue with runclock output

--- runclock.ps1
+++ runclock.ps1
@@ -10,10 +10,10 @@ $toolsDir = Join-Path $env:USERPROFILE "tools"
 New-Item -ItemType Directory -Path $toolsDir -Force | Out-Null
 
 # Restore packages
-dotnet restore .\DesktopClock\DesktopClock.csproj -r win-x64
+
 
 # The build command
-$publishCmd = "msbuild .\DesktopClock\DesktopClock.csproj /t:publish /p:PublishDir=$toolsDir /p:Configuration=Release /p:RuntimeIdentifier=win-x64"
+$publishCmd = "dotnet restore .\DesktopClock\DesktopClock.csproj -r win-x64 && msbuild .\DesktopClock\DesktopClock.csproj /t:publish /p:PublishDir=$toolsDir /p:Configuration=Release /p:RuntimeIdentifier=win-x64"
 
 # Run publish and capture output only if it fails
 $null = iex $publishCmd
